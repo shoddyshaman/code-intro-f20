f52be2947f9de56c8e5b7c84eba5f630947cade2
shoddyshaman <dhananjoy.tiwari@gmail.com>
2022-03-01 15:28:18 -0700

    finsihed coding all parts

--- calc.js
+++ calc.js
@@ -1,23 +1,33 @@
-const readline = require('readline');
+const readline = require("readline");
 
 const reader = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+reader.question("What would you like to calculate?", function (input) {
+  tokens = input.split(" ");
 
+  mathSymbol = tokens[0];
+  num1 = Number(tokens[1]);
+  num2 = Number(tokens[2]);
 
-reader.question("What would you like to calculate?", function(input){
-	tokens = input.split(' ');
-	
-	mathSymbol = tokens[0];
-	num1 = Number(tokens[1]);
-	num2 = Number(tokens[2]);
+  if (mathSymbol === "+") {
+    console.log(num1 + num2);
+  } else if (mathSymbol === "-") {
+    console.log(num1 - num2);
+  } else if (mathSymbol === "*") {
+    console.log(num1 * num2);
+  } else if (mathSymbol === "/") {
+    console.log(num1 / num2);
+  } else if (mathSymbol === "root") {
+    console.log(Math.sqrt(num1));
+  } else if (mathSymbol === "pow") {
+    console.log(Math.pow(num1, num2));
+  } else {
+    console.log("Please try again");
+  }
 
-	
-
-
-	// This line closes the connection to the command line interface.
-	reader.close()
-
+  // This line closes the connection to the command line interface.
+  reader.close();
 });
